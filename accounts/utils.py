498badb83ebda80ae5a46eb5ff200077d84125fd
Fantine NYIRANDIKUBWIMANA <fantine@Fantines-MBP.lan>
2024-06-24 00:07:35 -0400

    add utils

---
+++ accounts/utils.py
@@ -0,0 +1,36 @@
+from django.conf import settings
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+def detectUser(user):
+    redirectUrl = 'vendorDashboard' if user.role == 1 else 'customerDashboard' if user.role == 2 else '/admin'
+    return redirectUrl
+
+def send_email(request, user, mail_subject_text, render_string):
+    from_email = settings.DEFAULT_FROM_EMAIL
+    #current_site is the domain name
+    current_site = get_current_site(request)
+    mail_subject = mail_subject_text
+    message = render_to_string(render_string, {
+    'user': user,
+    'domain': current_site,
+    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+    'token': default_token_generator.make_token(user),
+    })
+    to_email = user.email
+    mail = EmailMessage(mail_subject, message, from_email, to=[to_email])
+    mail.send()
+    
+
+def send_verification_email(request, user):
+    send_email(request, user, 'Account activation from OnlineFood', 'accounts/emails/account_verification_email.html')
+    
+
+
+def send_password_reset_email(request, user):
+    send_email(request, user, 'Reset password on OnlineFood', 'accounts/emails/account_reset_password.html')
+
