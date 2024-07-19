cc267cac4bafe21c4d7818cd41f8dd3ec570b271
Fantine <ndilillfann@gmail.com>
2024-07-19 16:54:11 -0400

    Changes to forms

--- vendor/forms.py
+++ vendor/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 
-from vendor.models import Vendor
+from vendor.models import OpeningHour, Vendor
 
 class VendorForm(forms.ModelForm):
     vendor_license = forms.FileField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
     class Meta:
         model = Vendor
         fields = ['vendor_name', 'vendor_license']
+
+class OpeningHourForm(forms.ModelForm):
+    class Meta:
+        model = OpeningHour
+        fields = ['day', 'from_hour', 'to_hour', 'is_closed']
+    
+    
