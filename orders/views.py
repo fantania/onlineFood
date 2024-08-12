835246c08761d3a9f682e828451e22143348d87d
Fantine <ndilillfann@gmail.com>
2024-08-12 15:07:16 -0400

    paypal payment

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 from marketplace.context_processors import get_cart_amounts
@@ -38,8 +39,15 @@ def place_order(request):
             order.save() # order id/pk is generated
             order.order_number = generate_order_number(order.id)
             order.save() # save the generated order number with the id/pk
-            return redirect('place_order')
+            context = {
+                'order': order,
+                'cart_items': cart_items,
+            }
+            return render(request, 'orders/place_order.html', context)
         else:
             print(form.errors)
         
     return render(request, 'orders/place_order.html')
+
+def payments(request):
+    return HttpResponse('Payment view')
