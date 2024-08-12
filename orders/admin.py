961eda0a325adec79f4cb94a4106e5885d03fe91
Fantine <ndilillfann@gmail.com>
2024-08-12 16:55:38 -0400

    Payment

--- orders/admin.py
+++ orders/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from orders.models import Order, OrderedFood, Payment
 
+class OrderderFoodInline(admin.TabularInline):
+    model = OrderedFood
+    readonly_fields = ('order', 'payment', 'user', 'food_item')
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'name', 'phone', 'email', 'total', 'payment_method', 'status', 'is_ordered']
+    inlines = [OrderderFoodInline]
+
 admin.site.register(Payment)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderedFood)
--- orders/views.py
+++ orders/views.py
@@ -4,8 +4,12 @@ from django.shortcuts import redirect, render
 from marketplace.context_processors import get_cart_amounts
 from marketplace.models import Cart
 from orders.forms import OrderForm
-from orders.models import Order
+from orders.models import Order, OrderedFood, Payment
 from orders.utils import generate_order_number
+
+
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
 
 def place_order(request):
     cart_items = Cart.objects.filter(user=request.user).order_by('created_date')
@@ -50,4 +54,48 @@ def place_order(request):
     return render(request, 'orders/place_order.html')
 
 def payments(request):
+    # Check if the request is ajax or not
+    if is_ajax(request) and request.method == 'POST':
+        # STORE THE PAYMENT DETAILS IN THE PAYMENT MODEL
+        order_number = request.POST.get('order_number')
+        transaction_id = request.POST.get('transaction_id')
+        payment_method = request.POST.get('payment_method')
+        status = request.POST.get('status')
+
+        order = Order.objects.get(user=request.user, order_number = order_number)
+        payment = Payment(
+            user = request.user,
+            payment_method = payment_method,
+            transaction_id = transaction_id,
+            amount = order.total,
+            status = status
+        )
+
+        payment.save()
+
+        # UPDATE THE ORDER
+        order.payment = payment
+        order.is_ordered = True
+        order.save()
+
+       # MOVE THE CART ITEMS TO ORDERED FOOD MODEL
+        cart_items =  Cart.objects.filter(user=request.user)
+        for item in cart_items:
+            orderder_food = OrderedFood()
+            orderder_food.order = order
+            orderder_food.payment = payment
+            orderder_food.user = request.user
+            orderder_food.food_item = item.foodItem
+            orderder_food.quantity = item.quantity
+            orderder_food.price = item.foodItem.price
+            orderder_food.amount = item.foodItem.price * item.quantity # total amount
+            orderder_food.save()
+
+    # SEND ORDER CONFIRMATION EMAIL TO THE CUSTOMER
+
+    # SEND ORDER RECEIVED EMAIL TO THE VENDOR
+
+    # CLEAR THE CART IF THE PAYMENT IS SUCCESS
+
+    # RETURN BACK TO AJAX WITH THE STATUS SUCCESS OF FAILURE
     return HttpResponse('Payment view')
